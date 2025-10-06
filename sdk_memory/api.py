367e8b40380d0e5fb12131eb0f8f4c4d32ca23f8
Brett <68360370+Brettict@users.noreply.github.com>
2025-10-06 17:19:07 +0100

    Update api.py

--- sdk_memory/api.py
+++ sdk_memory/api.py
@@ -3,20 +3,21 @@ import time
 
 rec = RtspRecorder(
     rtsp_url="rtsp://192.168.137.99:50001/live/0",
-    ffmpeg_path=r"C:\Users\vq24975\Downloads\ffmpeg-7.0.2-full_build\ffmpeg-7.0.2-full_build\bin\ffmpeg.exe",
+    ffmpeg_path=r"C:\Users\vq24975\Downloads\ffmpeg-7.0.2-full_build\ffmpeg-7.0.2-full_build\bin\ffmpeg.exe", # use your own ffmpeg path
     save_dir="recording",
     mode="memory",
-    buffer_size=60  # 缓存最近 60 秒
+    buffer_size=60  # buffer save latest 60-second length video
 )
 
 
 rec.start()
 time.sleep(20)
 
-# 导出最近 10 秒视频
+# output 10 last 10 seconds video
 rec.dump("last10s.ts", start=-10, end=0)
 
-# 导出最近 30 到最近 15 秒之间的视频
+# output video between the latest 30s and 15s
 rec.dump("mid_clip.ts", start=-30, end=-15)
 
 rec.stop()
+
